9fdc6bb55f939576f732e26ffc7d0d37e4b64cf7
amirahnasihah <amirahnasihah97@gmail.com>
2023-08-25 02:42:13 +0800

    middleware

---
+++ 03-introduction-to-express/express-simplified/routes/users.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET request */
+router.get("/", (req, res) => {
+  res.send("User list");
+});
+
+// static, always put it above dynamic routes.
+router.get("/new", (req, res) => {
+  res.send("User new form");
+});
+
+/*
+- POST request - can put in different file.
+- Express read from top to bottom.
+- that's static make sure you always put it above your dynamic routes.
+*/
+router.post("/", (req, res) => {
+  res.send("Create User");
+});
+
+/*
+Another method called the `route`. To pass the path and chain together all http request.
+- kind of cleans up the code.
+*/
+router
+  .route("/:userId")
+  .get((req, res) => {
+    console.log(req.user);
+    // it's pulling that number directly from url
+    res.send(`Get User with ID ${req.params.userId}`);
+  })
+  .put((req, res) => {
+    res.send(`Update User with ID ${req.params.userId}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete User with ID ${req.params.userId}`);
+  });
+
+// Advanced Routing - dynamic parameter. ex; user's id
+router.get("/:userId", (req, res) => {
+  // it's pulling that number directly from url
+  res.send(`Get User with ID ${req.params.userId}`);
+});
+
+/* PUT request */
+router.put("/:userId", (req, res) => {
+  res.send(`Update User with ID ${req.params.userId}`);
+});
+
+/* DELETE request */
+router.delete("/:userId", (req, res) => {
+  res.send(`Delete User with ID ${req.params.userId}`);
+});
+
+/* `.param` + middleware
+
+- this function is going to run any time it finds a param that matches the name you pass in.
+- saying hey whenever you find a parameter with the name of id i want you to run this function and this function is going to take `request`, `response`, `next` property and take the value of the thing in our case the `id` itself
+*/
+const users = [{ name: "Ludwig" }, { name: "Mozart" }];
+router.param("userId", (req, res, next, userId) => {
+  console.log(userId);
+  req.user = users[userId];
+  next();
+});
+
+module.exports = router;
--- 03-introduction-to-express/express-simplified/server.js
+++ 03-introduction-to-express/express-simplified/server.js
@@ -1,8 +1,10 @@
+/* source: https://www.youtube.com/watch?v=SccSCuHhOw0 */
 const express = require("express");
 const app = express(); // to setup actual server (npm run devStart)
 
 /* View Engine */
 app.set("view engine", "ejs");
+// app.use(logger);
 
 /* Route handler for a GET request */
 app.get("/", (request, response) => {
@@ -13,7 +15,28 @@ app.get("/", (request, response) => {
   // response.status(500).json({ message: "Error" }); // using this inside of some type of api and to send json to client
 
   // response.download("server.js"); // send file to user to download
-  response.render("index", { text: "World" }); // to render a file + object parameter
+  response.render("index", { text2321: "World" }); // to render a file + object parameter
 });
+
+/* Router - to create another instance of your application that is its own little mini application that has all of its own logic, let's all the code related to our users and put that inside of its own file `routes`:
+
+app.get("/users", (req, res) => {
+  res.send("User list");
+});
+
+app.get("/users/new", (req, res) => {
+  res.send("User new form");
+});
+*/
+const userRouter = require("./routes/users");
+app.use("/users", userRouter);
+
+/* Middleware
+- if you have a middleware that you want to use everywhere on all of your routes always define it at the very top of your page
+*/
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 app.listen(3000); // to start and run server
