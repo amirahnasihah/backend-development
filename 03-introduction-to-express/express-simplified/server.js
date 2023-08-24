20e89b4a9125f2e0dfb93332f59b50909ebab949
amirahnasihah <amirahnasihah97@gmail.com>
2023-08-25 01:28:23 +0800

    i ejs

--- 03-introduction-to-express/express-simplified/server.js
+++ 03-introduction-to-express/express-simplified/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const app = express(); // to setup actual server
+const app = express(); // to setup actual server (npm run devStart)
 
-// Route handler for a GET request
+/* View Engine */
+app.set("view engine", "ejs");
+
+/* Route handler for a GET request */
 app.get("/", (request, response) => {
   console.log("Here is a GET request");
   // response.sendStatus(500); // to send status
-  response.status(500).send("Hello"); // to send status and with message
+  // response.status(500).send("Hello"); // to send status and with message
   // response.send("Hello, world!"); // sending info to user
+  // response.status(500).json({ message: "Error" }); // using this inside of some type of api and to send json to client
+
+  // response.download("server.js"); // send file to user to download
+  response.render("index", { text: "World" }); // to render a file + object parameter
 });
 
 app.listen(3000); // to start and run server
