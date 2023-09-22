343a4d9e9bb4978bcc6b6846d7e6bc2a63a05d33
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-09-22 23:17:42 +0800

    update higher-order-array.js

--- 01-es6-review-intro/higher-order-array.js
+++ 01-es6-review-intro/higher-order-array.js
@@ -2,5 +2,9 @@
 higher-order array: filter(), map(), sort(), find()
 */
 
-// filter
+// filter to find odd numbers
 const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const oddNumbers = arr.filter((e) => {
+	return e 
+})
