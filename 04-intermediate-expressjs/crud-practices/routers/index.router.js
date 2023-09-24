adfa0c18167e6fe8e1d39c88b9cfb4b00730dfde
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-09-25 04:20:48 +0800

    update 04-intermediate-expressjs/crud-practices/routers/index.router.js

--- 04-intermediate-expressjs/crud-practices/routers/index.router.js
+++ 04-intermediate-expressjs/crud-practices/routers/index.router.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
+const { getRoot } = require("../controllers/index.controllers");
 
 const router = Router();
 
-router.route("/").get();
+router.route("/").get(getRoot);
 
 module.exports = router;
