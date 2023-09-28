ee933c45b98b352c48b157b38835bf79b2d3d92b
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-09-29 02:30:02 +0800

    update pyramid.js

--- 04-intermediate-expressjs/pyramid.js
+++ 04-intermediate-expressjs/pyramid.js
@@ -1,30 +1,17 @@
 /*
 Create an Express API which has one route “/pyramid/:n” which creates a pyramid with “n” rows, for example visiting “/pyramid/5” will render.
 
-```
 *
 * *
 * * *
 * * * *
 * * * * *
-```
 
+*/
+
+
+/*
 row1
 row1, col1
 row1, col1, col 2
 */
-
-let heightRow = 5
-
-for (var i = 1; i <= heightRow; i++) {
-	
-	let row = ""
-	
-	for (var j = 1; j <= i; j++) {
-		
-		row += "#"
-		
-	}
-	
-	console.log(row)
-}
