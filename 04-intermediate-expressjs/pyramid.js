110e24339ffcb00e251f673abd61071fb891873e
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-09-29 02:23:20 +0800

    update pyramid.js

--- 04-intermediate-expressjs/pyramid.js
+++ 04-intermediate-expressjs/pyramid.js
@@ -17,8 +17,8 @@ row1, col1, col 2
 let star = 5
 
 for (var i = 1; i <= star; i++) {
-	for (var j = 1; j <= star; j++) {
-		console.log(i + " " + j)
+	for (var j = 1; j <= i; j++) {
+		console.log(5 += "g")
 	}
 	
 }
