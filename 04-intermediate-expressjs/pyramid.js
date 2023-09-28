c437b6a9e1fcccedc3e397122b4479d5ce1fd5a4
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-09-28 12:19:44 +0800

    update pyramid.js

--- 04-intermediate-expressjs/pyramid.js
+++ 04-intermediate-expressjs/pyramid.js
@@ -12,10 +12,9 @@ Create an Express API which has one route “/pyramid/:n” which creates a pyra
 
 const pyramid = (n) => {
 	for (let i = 0;  i < 5; i++) {
-		//console.log(n)
-		if (i ) {
-			// 
-			console.log(n)
+		console.log(i)
+		for (var j = 0; j < j.length; j++) {
+			console.log(j)
 		}
 	}
 }
