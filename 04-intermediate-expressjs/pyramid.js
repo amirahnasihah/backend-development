757d6d6618668854c8f117de434309515d4d33ad
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-10-02 21:59:23 +0800

    update pyramid.js

--- 04-intermediate-expressjs/pyramid.js
+++ 04-intermediate-expressjs/pyramid.js
@@ -16,16 +16,17 @@ let numsRow = 5;
 
 for (var i = 1; i <= numsRow; i++) {
 	let row = " "
-	console.log(row)
+	// console.log(row)
 	
 	for (var j = 1; j <= i; j++) {
 		// now at row1 i = 1, j = 1; 1 <= 1;
 		let withinRow = "j"
 		// console.log(withinRow)
-		console.log(row += withinRow)
+		result = row += withinRow
+		// console.log(row += withinRow)
 	}
 	
-	// console.log("end:", row)
+	console.log("end:", row)
 }
 
 
