c872a4719b3da07e45775ec93c52125931365c35
amirahnasihah <amirahnasihah97@gmail.com>
2023-10-04 04:09:05 +0800

    EJS

--- 04-intermediate-expressjs/templating-with-ejs/controllers/index.js
+++ 04-intermediate-expressjs/templating-with-ejs/controllers/index.js
@@ -1,10 +1,11 @@
+/* INDEX */
 const indexRoute = (req, res) => {
   // res.send("hi!!");
   // "index" name should be same as "index.ejs" views name.
   res.render("index", { title: "Hi there!" });
 };
 
-// self experiment. on how to use JSON objects and arrays as dynamic values in EJS templates //
+/* // self experiment. on how to use JSON objects and arrays as dynamic values in EJS templates // */
 const profileRoute = (req, res) => {
   const user = {
     name: "John Doe",
