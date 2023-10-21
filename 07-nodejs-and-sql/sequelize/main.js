3eef56eb32c44de7df30a1aaf042cf224b048ac1
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-10-22 02:38:54 +0800

    update 07-nodejs-and-sql/sequelize/main.js0

--- 07-nodejs-and-sql/sequelize/main.js
+++ 07-nodejs-and-sql/sequelize/main.js
@@ -1,1 +1,9 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({
+  dialect: "mysql2",
+  host: "127.0.0.1",
+  username: "root",
+  password: "pass",
+  port: 3306
+})
