54e32bfdeee4aba2fca788a2186c22b80a2297dd
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-02 04:45:07 +0800

    update main.js

--- 07-nodejs-and-sql/sequelize-basic/main.js
+++ 07-nodejs-and-sql/sequelize-basic/main.js
@@ -1,7 +1,6 @@
-// 0. enable Sequelize service
+// 0.
 const { Sequelize, DataTypes, Op } = require("sequelize");
 
-// 1. initiate connection with sql
 const db = new Sequelize({
   dialect: "mysql",
   host: "127.0.0.1",
