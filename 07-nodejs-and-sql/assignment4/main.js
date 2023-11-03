9ad1a4aa76a4be6805b692e473c854a4b31291b7
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-03 23:30:09 +0800

    update main.js

--- 07-nodejs-and-sql/assignment4/main.js
+++ 07-nodejs-and-sql/assignment4/main.js
@@ -7,3 +7,17 @@ const sequelize = new Sequelize({
 	port: 3306,
 	database: "assignment4",
 });
+
+const run = async () => {
+	try {
+		await sequelize.authenticate();
+		await sequelize.sync();
+		console.log("database connection succeed!")
+		
+		// sql queries
+	}
+	catch(error) {
+		console.log(error.message)
+	}
+};
+run();
