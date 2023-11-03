a3f5cf6a58847e214f835732b46974201ca29fe5
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-03 23:29:19 +0800

    update main.js

--- 07-nodejs-and-sql/assignment4/main.js
+++ 07-nodejs-and-sql/assignment4/main.js
@@ -1,1 +1,9 @@
- const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+	dialect: "mysql",
+	host: "root",
+	password: "password1",
+	port: 3306,
+	database: "assignment4",
+});
