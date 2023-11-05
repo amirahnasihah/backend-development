ea76721e21d057dee5f8bc3d061e5dc495c5fbee
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-06 03:33:34 +0800

    update 07-nodejs-and-sql/library-project-intro/routers/book.router.js

--- 07-nodejs-and-sql/library-project-intro/routers/book.router.js
+++ 07-nodejs-and-sql/library-project-intro/routers/book.router.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
+const { createBookHandler } = require("../controllers/book.controller");
 
 const router = Router();
 
 // router.route("/").post(<./controllers>)
-router.route("/").post();
+router.route("/").post(createBookHandler);
 
 module.exports = router;
