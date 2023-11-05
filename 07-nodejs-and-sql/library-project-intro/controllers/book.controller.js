fff7521077fe7c58b6c61167eacc48a4fd463409
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-06 03:31:04 +0800

    update 07-nodejs-and-sql/library-project-intro/controllers/book.controller.js

--- 07-nodejs-and-sql/library-project-intro/controllers/book.controller.js
+++ 07-nodejs-and-sql/library-project-intro/controllers/book.controller.js
@@ -2,9 +2,9 @@ const asyncHandler = require("express-async-handler");
 
 const createBookHandler = asyncHandler(async (req, res) => {
   try {
-    console.log("get data list");
-    res.json({ msg: "hi" });
-    res.status(200);
+    console.log("create new book");
+    const { title, author } = req.body;
+    const book = await createNewBook({ title, author });
   } catch (error) {
     res.status(500);
     throw new Error(error.message);
