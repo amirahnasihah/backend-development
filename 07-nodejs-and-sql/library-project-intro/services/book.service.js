31c5040514e14ca7f22ab7769020db3ca80f9cfa
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-06 02:33:59 +0800

    update 07-nodejs-and-sql/library-project-intro/services/book.service.js

--- 07-nodejs-and-sql/library-project-intro/services/book.service.js
+++ 07-nodejs-and-sql/library-project-intro/services/book.service.js
@@ -1,1 +1,6 @@
-// operations that wants to perform in books
+// operations that wants to perform in books:
+// find books
+// find book by id
+// handle borrowing logic
+// create a new book
+// CRUD operations
