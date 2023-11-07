51fff911bec8fa16fec074be57d9f94032f289d8
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-08 03:45:32 +0800

    update 07-nodejs-and-sql/library-project-intro/server.js

--- 07-nodejs-and-sql/library-project-intro/server.js
+++ 07-nodejs-and-sql/library-project-intro/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/error.middleware");
 const { PORT, NODE_ENV } = require("./config/secrets");
 const { router } = require("./routers");
 const { db } = require("./models");
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", router);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   db.authenticate();
