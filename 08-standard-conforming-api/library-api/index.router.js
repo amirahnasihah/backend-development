4a478e32f67c7063063de0a3d4e9cddb8c268eef
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-16 23:41:38 +0800

    update index.router.js

--- 08-standard-conforming-api/library-api/index.router.js
+++ 08-standard-conforming-api/library-api/index.router.js
@@ -2,7 +2,10 @@
 const { Router } = require('express');
 const router = Router();
 
-// swagger js
+// swagger js config require
+const { } = require("./config/swagger.config");
+
+
 
 // ... router.use("/api", )
 router.use("/docs", swagger.)
