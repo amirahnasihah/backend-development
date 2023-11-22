b2f441511088f5e1fe7fc86cc6e02158f6120050
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-22 22:15:17 +0800

    update book.router.js

---
+++ 08-standard-conforming-api/library-api/book.router.js
@@ -0,0 +1,78 @@
+/**
+ * @openapi
+ * "/api/books/":
+ *   get:
+ *     tags:
+ *       - Books
+ *     summary: Get all the books
+ *     responses:
+ *       200:
+ *         description: Success, returns an array of all the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/BookDto"
+ *       500:
+ *         description: Internal server error occurred
+ *   post:
+ *     tags:
+ *       - Books
+ *     summary: Create a new book
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json
+ *           schema:
+ *             $ref: "#/components/schemas/CreateBookDto"
+ *     responses:
+ *       201:
+ *         description: Accepted, created new book
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: "#/components/schemas/BookDto"
+ * 
+ * /api/books/{id}:
+ *   patch:
+ *     tags:
+ *       - Books
+ *     summary: update book by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json
+ *           schema:
+ *             $ref: "#/components/schemas/UpdateBookDto"
+ *     responses:
+ *       202:
+ *         description: Successfully updated the book
+ *         content:
+ *           application/json
+ *             schema:
+ *               $ref: "#/components/schemas/BookDto"
+ *   delete:
+ *     tags:
+ *       - Books
+ *     summary: delete book by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       202:
+ *         description: Successfully updated the book
+ *         content:
+ *           application/json
+ *             schema:
+ *               $ref: "#/components/schemas/BookDto"
+ */
