a84809c3cfa245016cf7ab8be68488643ff58817
amirahnasihah <amirahnasihah97@gmail.com>
2023-11-26 19:38:12 +0800

    openapi

--- 08-standard-conforming-api/library-api/book.model.js
+++ 08-standard-conforming-api/library-api/book.model.js
@@ -4,8 +4,34 @@ const bookModel = (db) => {
   return db.define(Book, {
     id: {
       type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
   });
 };
+
+// OPEN API MODEL SPEC
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    CreateBookDto:
+ *      type: object
+ *      properties:
+ *        title:
+ *          type: string
+ *          default: An amazing book
+ *        author:
+ *
+ */
 
 module.exports = { bookModel };
--- 08-standard-conforming-api/library-api/index.router.js
+++ 08-standard-conforming-api/library-api/index.router.js
@@ -1,14 +1,14 @@
 // routers/index.js
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
+// Bind Swagger to Router
 // swagger js config require
-const swaggerUi = require("swagger-ui-express")
+const swaggerUi = require("swagger-ui-express");
 const { swaggerSpecification } = require("./config/swagger.config");
-
 
 // ... router.use("/api", bookController)
 router.use("/docs", swaggerUi.serve);
 router.use("/docs", swaggerUi.setup(swaggerSpecification));
 
-module.exports = router;
+module.exports = { router };
