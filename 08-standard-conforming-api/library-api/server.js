0c4c080fedfffda1cf359d414ed3f73bd4b57726
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-11-27 22:06:46 +0800

    update 08-standard-conforming-api/library-api/server.js

--- 08-standard-conforming-api/library-api/server.js
+++ 08-standard-conforming-api/library-api/server.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use("/api", routers);
 
 // Basic route for the root URL "/"
-app.route("/").get( (req, res, next) => {
-  res.json({ msg: "OK" });
-});
+// app.route("/").get( (req, res, next) => {
+//  res.json({ msg: "OK" });
+// });
 
 app.listen(port, () => {
   console.log("server is live!");
